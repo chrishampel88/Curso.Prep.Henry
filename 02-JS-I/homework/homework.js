2f9fcbd222e3c540283e02290cf47617ee487741
Christian Hampel <christianhampel88@gmail.com>
2021-07-08 23:54:55 -0300

    finish homeworh js-II

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -158,7 +158,7 @@ function esPositivo(numero) {
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
-  // Ejemplo: "hello world" pasaría a ser "hello world!"
+  // Ejemplo: "hello world" pasaría a ser "hello world!"gi
   // Tu código:
   return str + "!";
 }
